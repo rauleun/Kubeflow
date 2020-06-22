14cd8626b93c1e783b4ce2f3af6f11f16ff7c976
hyperbrain <rauleun@gmail.com>
2020-06-22 19:47:57 +0900

    kfserving

--- KFserving/kfserving_output.py
+++
@@ -1,9 +0,0 @@
-import json
-import numpy as np
-import cv2
-from PIL import Image
-
-with open("ocr_det_output.json") as f:
-    preds = json.loads(f.read())["predictions"]
-    preds = np.array(preds) 
-    cv2.imwrite('ocr_det_output.png',np.squeeze(preds)) 
